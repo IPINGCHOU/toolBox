573f19d859a68c72a7fd7a4767176ace57285715
IPINGCHOU <f77252426@gmail.com>
2023-06-27 01:18:34 +0900

    add mofa table

--- kanjiString.py
+++ kanjiString.py
@@ -1,5 +1,6 @@
 
 import pykakasi
+import kanjiStringUtils
 
 
 class kanjiTools:
@@ -33,4 +34,57 @@ class kanjiTools:
         res =  len(set(g1).intersection(set(g2)))/len(set(g1).union(set(g2)))
         
         return res
-        
+    
+
+    def toRomaji_MOFA(self, kana):
+        out = []
+        pt = 0
+        doubleConsonant = False # 促音 check
+        while pt < len(kana):
+            print(pt, kana[pt])
+            # 2 kanas
+            if pt + 2 <= len(kana) and kana[pt:pt+2] in kanjiStringUtils.hebon:
+                match = kanjiStringUtils.hebon[kana[pt:pt+2]]
+                if doubleConsonant == True and kana[pt] == "チ":
+                    doubleConsonant = False
+                    match = "t" + match
+                out.append(match)
+                pt += 2
+            # found "ッ"
+            elif kana[pt] == "ッ":
+                doubleConsonant = True
+                pt += 1
+            # found "ー", skip
+            elif kana[pt] == "ー":
+                pt += 1
+            # 1 kana
+            elif kana[pt] in kanjiStringUtils.hebon:
+                match = kanjiStringUtils.hebon[kana[pt]]
+                if doubleConsonant == True:
+                    doubleConsonant = False
+                    if kana[pt] == "チ":
+                        match = "t" + match
+                    else:
+                        match = match[0] + match
+                # 撥音 check
+                if match[0] in {"B", "M", "P"} and len(out) > 0 and out[-1] == "N":
+                    out[-1] = "M"
+                # 長音 check OやUは記入しません。
+                if match == "U" and ((out[-1][-1] == "U") or out[-1][-1] == "O") :
+                    # OU, UU skip
+                    match = ""
+                elif match == "O" and len(out) > 0 and out[-1] == "O":
+                    # OO skip
+                    match = ""
+
+                out.append(match)
+                pt += 1
+            else:
+                out.append("X")
+                pt += 1
+
+        out = "".join(out)
+        out = out.lower()
+        return out
+
+                
--- test_kanjiString.py
+++ test_kanjiString.py
@@ -86,3 +86,115 @@ def test_gram():
     assert uni == ["東", "京", "大" , "学"]
     assert bi == ["東京", "京大", "大学"]
     assert tri == ["東京大", "京大学"]
+
+def test_toRomaji_MOFA():
+    toolBox = kanjiString.kanjiTools()
+    # normal
+    s1 = "カタカナ"
+    s1t = toolBox.toRomaji_MOFA(s1)
+    assert s1t == "katakana"
+
+    s2 = "シュヒュルロニョ"
+    s2t = toolBox.toRomaji_MOFA(s2)
+    assert s2t == "shuhyururonyo"
+
+    # 撥音
+    s3, s4, s5 = "ナンバ", "ホンマ", "サンペイ"
+    s3t = toolBox.toRomaji_MOFA(s3)
+    s4t = toolBox.toRomaji_MOFA(s4)
+    s5t = toolBox.toRomaji_MOFA(s5)
+
+    assert s3t == "namba"
+    assert s4t == "homma"
+    assert s5t == "sampei"
+
+    # 促音
+    s6, s7, s8, s9 = "ハットリ", "キッカワ", "ホッチ", "ハッチョウ"
+    s6t = toolBox.toRomaji_MOFA(s6)
+    s7t = toolBox.toRomaji_MOFA(s7)
+    s8t = toolBox.toRomaji_MOFA(s8)
+    s9t = toolBox.toRomaji_MOFA(s9)
+
+    assert s6t == "hattori"
+    assert s7t == "kikkawa"
+    assert s8t == "hotchi"
+    assert s9t == "hatcho"
+    
+    # 「―」を省略する場合
+    s10, s11, s12 = "ニーナ", "シーナ", "サリー"
+    s10t = toolBox.toRomaji_MOFA(s10)
+    s11t = toolBox.toRomaji_MOFA(s11)
+    s12t = toolBox.toRomaji_MOFA(s12)
+
+    assert s10t == "nina"
+    assert s11t == "shina"
+    assert s12t == "sari"
+
+    # 「イ」を省略しない場合
+    s13, s14, s15 = "ニイナ", "シイナ", "サリイ"
+    s13t = toolBox.toRomaji_MOFA(s13)
+    s14t = toolBox.toRomaji_MOFA(s14)
+    s15t = toolBox.toRomaji_MOFA(s15)
+
+    assert s13t == "niina"
+    assert s14t == "shiina"
+    assert s15t == "sarii"
+
+    # 「ウ」を含む長音「ウウ」の場合（「UU」は表記しません。）
+    s16, s17, s18 = "ヒュウガ", "ユウキ", "ユウコ"
+    s16t = toolBox.toRomaji_MOFA(s16)
+    s17t = toolBox.toRomaji_MOFA(s17)
+    s18t = toolBox.toRomaji_MOFA(s18)
+
+    assert s16t == "hyuga"
+    assert s17t == "yuki"
+    assert s18t == "yuko"
+
+    # 「オ」を含む長音「オウ」の場合（「OU」は表記しません。）
+    s19, s20, s21 = "コウタ", "ヨウコ", "リョウコ"
+    s19t = toolBox.toRomaji_MOFA(s19)
+    s20t = toolBox.toRomaji_MOFA(s20)
+    s21t = toolBox.toRomaji_MOFA(s21)
+
+    assert s19t == "kota"
+    assert s20t == "yoko"
+    assert s21t == "ryoko"
+
+    # 「オ」を含む長音「オオ」の場合（「OO」は表記しません。）
+    s22, s23, s24 = "オオノ", "オオコウチ", "オオニシ"
+    s22t = toolBox.toRomaji_MOFA(s22)
+    s23t = toolBox.toRomaji_MOFA(s23)
+    s24t = toolBox.toRomaji_MOFA(s24)
+
+    assert s22t == "ono"
+    assert s23t == "okochi"
+    assert s24t == "onishi"
+
+    # 末尾が「オオ」音で、ヨミカタが「オ」の場合（「OO」と表記します。）
+    s25, s26, s27 = "セノオ", "タカトオ", "ヨコオ"
+    s25t = toolBox.toRomaji_MOFA(s25)
+    s26t = toolBox.toRomaji_MOFA(s26)
+    s27t = toolBox.toRomaji_MOFA(s27)
+
+    assert s25t == "senoo"
+    assert s26t == "takatoo"
+    assert s27t == "yokoo"
+
+    # 末尾が「オウ」音で、ヨミカタが「ウ」の場合（「OU」とは表記しません。）
+    s28, s29, s30 = "イトウ", "タカトウ", "ミソノウ"
+    s28t = toolBox.toRomaji_MOFA(s28)
+    s29t = toolBox.toRomaji_MOFA(s29)
+    s30t = toolBox.toRomaji_MOFA(s30)
+
+    assert s28t == "ito"
+    assert s29t == "takato"
+    assert s30t == "misono"
+
+    # 「ヴ」のつく氏名例
+    s31, s32 = "ヴィヴィアン", "ヴォードレール"
+    s31t = toolBox.toRomaji_MOFA(s31)
+    s32t = toolBox.toRomaji_MOFA(s32)
+
+    assert s31t == "buibuian"
+    assert s32t == "buodoreru"
+    
